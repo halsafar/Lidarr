72f53c556a5f433e4f238388234c9bb18e90e438
Halsafar <421822+halsafar@users.noreply.github.com>
2026-02-23 17:48:09 +0000

    Fix migration incase this PR is accepted.

--- src/NzbDrone.Core/Datastore/Migration/079_relax_not_null_constraints_extra_files.cs
+++ src/NzbDrone.Core/Datastore/Migration/079_relax_not_null_constraints_extra_files.cs
@@ -3,12 +3,18 @@ using NzbDrone.Core.Datastore.Migration.Framework;
 
 namespace NzbDrone.Core.Datastore.Migration
 {
-    [Migration(2026022001)] // Using a very high number until this PR is accepted
+    [Migration(2026022001)]
     public class relax_not_null_constraints_extra_files : NzbDroneMigrationBase
     {
         protected override void MainDbUpgrade()
         {
-            Alter.Table("ExtraFiles").AlterColumn("TrackFileId").AsInt32().Nullable();
+            if (Schema.Table("ExtraFiles").Exists())
+            {
+                Alter.Table("ExtraFiles")
+                    .AlterColumn("TrackFileId")
+                    .AsInt32()
+                    .Nullable();
+            }
         }
     }
 }
